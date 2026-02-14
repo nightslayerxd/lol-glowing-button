06a2a9c50e8ce6d123f6754458c10aa4fcb1705e
nightslayerxd <annirudhkrishna1@gmail.com>
2026-02-14 15:47:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-left').onclick = () => {
+    alert("Thanks for supporting!");
+};
+document.getElementById('btn-right').onclick = () => {
+    alert("We're sorry to hear that.");
+};
